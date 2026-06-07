bbbec27c625fb29c436edf97374dac9b63e32bf6
Gabriel Soares Cassimiro <gsoarescassimiro@gmail.com>
2026-06-07 02:34:38 -0300

    🔧refactor(refat): retirado arquivos do projeto antigo

--- OrganizerBackEnd/OrganizerBackEnd/Dto/CreateListaDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace OrganizerBackEnd.Dto;
-
-public class CreateListaDto
-{
-    [Required(AllowEmptyStrings = false, ErrorMessage = "O Nome da lista não Pode ser Nulo")]
-    public string Nome { get; set; }
-}
