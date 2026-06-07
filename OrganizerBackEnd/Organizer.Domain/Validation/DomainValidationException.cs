ade963e4f08ccb95accb792c063b7887fb196f76
Gabriel Soares Cassimiro <gsoarescassimiro@gmail.com>
2026-06-07 00:49:16 -0300

    🔧refactor(domain): criado custo exception para o dominio

---
+++ OrganizerBackEnd/Organizer.Domain/Validation/DomainValidationException.cs
@@ -0,0 +1,12 @@
+namespace Organizer.Domain.Validation;
+
+public class DomainValidationException:Exception
+{
+    public DomainValidationException(string error):base(error) { }
+        
+    public static void When(bool hasErro,string error)
+    {
+        if(hasErro)
+            throw new DomainValidationException(error);
+    }
+}
